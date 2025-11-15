20b0f39b50a541e9353f04ade2ea504f068848ed
Julian Gutierrez <157057593+JulianGut2@users.noreply.github.com>
2025-11-14 21:45:14 -0600

    Add files via upload

--- NBA_API.py
+++ NBA_API.py
@@ -1,5 +1,18 @@
 # Retrieve general imports
-from nba_api.stats.endpoints import ShotChartDetail
+from nba_api.stats.endpoints import ShotChartDetail, BoxScoreSummaryV2
 from nba_api.live.nba.endpoints import scoreboard
 import pandas as pd
 import datetime
+
+# Create a list that contains all of the games in the data
+# Right now I'm gonna just establish the list
+games = []
+
+# Open and read the file then I'll just stuff it all into
+# the games list
+with open("yesterdays_games.txt", "r") as f:
+    for line in f:
+        games.append(line.strip())
+
+# Gonna keep this here for testing purposes
+# print(games)
